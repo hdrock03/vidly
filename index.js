2858e397c2d77d8ab9f98f0c4c695aa17d20a16d
hdrock03 <hardik.1rn16ec036@gmail.com>
2022-08-08 17:06:14 +0530

    mongoose connected

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+const mongoose = require('mongoose')
 const Joi = require('joi');
 const express = require('express');
 const app = express();
 const genres = require('./routes/genres')
 
-app.use(express.json()) // it returns the middleware function the job of this function is if there is json object in the body of the req object. it will parse the body of the req into json object. 
+mongoose.connect('mongodb://localhost/vidly')
+    .then(() => console.log('connected to mongodb'))
+    .catch(() => console.log('cannot connect to mongodb'))
 
+app.use(express.json()) 
 app.set('/api/genres' , genres)
 
 
