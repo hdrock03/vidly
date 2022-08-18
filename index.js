6b79038b5399bda8cf8c727e2c28245f521a811c
hdrock03 <hardik.1rn16ec036@gmail.com>
2022-08-19 00:40:14 +0530

    authentication done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const customers = require('./routes/customers')
 const movies = require('./routes/movies')
 const rentals = require('./routes/rentals')
 const users = require('./routes/users')
+const auth = require('./routes/auth')
 
 mongoose.connect('mongodb://localhost/vidly')
     .then(() => console.log('connected to mongodb'))
@@ -17,6 +18,7 @@ app.use('/api/customers', customers)
 app.use('/api/movies', movies)
 app.use('/api/rentals', rentals)
 app.use('/api/users', users)
+app.use('/api/auth', auth)
 
 
 const port = process.env.PORT || 3000
---
+++ routes/auth.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcrypt')
+const _ = require('lodash')
+const {User} = require('../models/user')
+const mongoose = require('mongoose')
+const express = require('express')
+const route = express.Router()
+const Joi = require('joi')
+
+route.post('/', async(req,res)=>{
+    console.log(req.body);
+    const {error} = validate(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
+
+    let user = await User.findOne({email: req.body.email})
+    console.log(user);
+    if(!user) return res.status(400).send('Invalid email or password')
+
+   const validPassword = bcrypt.compare(req.body.password, user.password)
+   if(!validPassword) return res.status(400).send('Invalid email or password')
+    res.send(true);
+
+})
+
+
+
+// this function is used for validating a new user
+function validate(req) {
+    const schema = {
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    }
+    return Joi.validate(req, schema)
+}
+
+module.exports = route
