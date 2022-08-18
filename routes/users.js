ff92e48e86668fb0c8f88ad2745b99f4fe020cd4
hdrock03 <hardik.1rn16ec036@gmail.com>
2022-08-18 22:41:34 +0530

    hashing completed

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt')
+const _ = require('lodash')
 const {User, validate} = require('../models/user')
 const mongoose = require('mongoose')
 const express = require('express')
@@ -13,14 +15,23 @@ route.post('/', async (req, res) => {
     if(user) return res.status(400).send('user already registered') // agr user phle se registered h to yeh execute hga
 
     // agr user registered nh h to user object me save kr denge
-    user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    })
+    // user = new User({ // instead of writing req.body so many times use lodash here
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: req.body.password
+    // })
+
+    user = new User(_.pick(req.body , ['name', 'email', 'password']))
+    const salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(user.password, salt)
 
     await user.save();
-    res.send(user)
+    // res.send({ // this is done because response in sending password which is not required and so istead of user we are sending name and email
+    //     name: user.name,
+    //     email: user.email
+    // })
+
+    res.send(_.pick(user, ['_id','name', 'email'])) // we are using pick method 1 argument is which object we need to operate and then 2nd which property we need to take
 })
 
 module.exports = route
