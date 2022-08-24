5470f4cd4700de7c0d149de6b300467e5a301d7b
hdrock03 <hardik.1rn16ec036@gmail.com>
2022-08-24 13:09:04 +0530

    setting response header

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+const config = require('config')
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const _ = require('lodash')
 const {User, validate} = require('../models/user')
@@ -31,7 +33,9 @@ route.post('/', async (req, res) => {
     //     email: user.email
     // })
 
-    res.send(_.pick(user, ['_id','name', 'email'])) // we are using pick method 1 argument is which object we need to operate and then 2nd which property we need to take
+    //setting up the header
+    const token = jwt.sign({_id : user._id}, config.get('jwtPrivateKey'))
+    res.header('x-auth-token', token).send(_.pick(user, ['_id','name', 'email'])) // we are using pick method 1 argument is which object we need to operate and then 2nd which property we need to take
 })
 
 module.exports = route
